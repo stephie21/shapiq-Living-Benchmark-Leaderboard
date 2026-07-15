0cb804d18513ab7a62a6acf84c9f316c0c833868
ZheShi <73885727+Zhe5602@users.noreply.github.com>
2026-07-15 11:34:46 +0200

    prepare for more runs

--- src/leaderboard/runner/benchmark_runner.py
+++ src/leaderboard/runner/benchmark_runner.py
@@ -100,11 +100,11 @@ def run_benchmark(
     aggregated_result = aggregate_fn(results)
 
     # print-out
-    logger.info("number of raw results: %d", len(results))
-    logger.info("First raw run record:")
-    logger.info(json.dumps(results[0], indent=2))
-    logger.info("\nAggregated result:")
-    logger.info(json.dumps(aggregated_result, indent=2))
+    # logger.info("number of raw results: %d", len(results))
+    # logger.info("First raw run record:")
+    # logger.info(json.dumps(results[0], indent=2))
+    # logger.info("\nAggregated result:")
+    # logger.info(json.dumps(aggregated_result, indent=2))
 
     return {
         "raw_results": results,
